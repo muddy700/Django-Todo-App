85c5863a0eee565dfd4b470462e4a95789f99cef
Muddy700 <mohamedmfaume700@gmail.com>
2021-01-24 13:27:44 -0800

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Button } from 'antd';
+
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Hey!, Cute Coder</h1>
+            <Button type="primary">Button</Button>
+    </div>
+  );
+}
+
+export default App;
